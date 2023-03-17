1b78cc14a401a32f71eae0f069a3b7e9359ae306
giorgi <gkiladze1993@gmail.com>
2023-03-18 02:39:42 +0400

    auto vercel

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css';
+
+const HeaderComponent = ({quantity}) => {
+
+	return (
+		<header className="header">
+			<div className="container">
+				<div className="header-area">
+					<h1 className="title">
+						<Link to="/">
+							products
+						</Link>
+					</h1>
+					<div className="cart-btn-content">
+						<Link to="/cart" className="cart-btn">
+							<img src="./img/shopping-cart.png" alt="cart"/>
+						</Link>
+						<span>
+							{quantity > 0 && quantity}
+						</span>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default HeaderComponent;
